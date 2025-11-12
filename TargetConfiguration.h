46129ef943e1e7d49a4e7a96228105f599f66949
grant <abernathy.grant@icloud.com>
2025-11-12 16:49:21 -0600

    Architecture determination

--- TargetConfiguration.h
+++ TargetConfiguration.h
@@ -115,6 +115,72 @@
 //
 // Determine the architecture.
 //
+#if defined(i386) || defined(__i386) || defined(__i386__) || defined(_M_IX86) || \
+    defined(__x86_64) || defined(__x86_64__) || defined(__amd64) || defined(__amd64__) || \
+    defined(_M_X64) || defined(_M_AMD64) || \
+    defined(__INTEL__)
+
+    #define CPU_X86 1
+
+    #if defined(__x86_64) || defined(__x86_64__) || defined(__amd64) || defined(__amd64__) || \
+        defined(_M_X64) || defined(_M_AMD64)
+        #define CPU_X86_64 1
+    #else
+        #define CPU_X86_32 1
+    #endif
+#endif
+
+#if defined(arm) || defined(__arm) || defined(__arm__) || defined(_M_ARM) || \
+    defined(arm64) || defined(__arm64) || defined(__arm64__) || defined(__aarch64__) || \
+    defined(__AARCH64EL__)
+
+    #define CPU_ARM 1
+
+    #if defined(arm64) || defined(__arm64) || defined(__arm64__) || defined(__aarch64__) || \
+        defined(__AARCH64EL__)
+        #define CPU_ARM64 1
+    #else
+        #define CPU_ARM32 1
+    #endif
+#endif
+
+#if defined(__ppc) || defined(__ppc__) || defined(__powerpc) || defined(__powerpc__) || \
+    defined(__ppc64) || defined(__ppc64__) || defined(__powerpc64) || defined(__powerpc64__) || \
+    defined(_M_PPC) || defined(_ARCH_PPC) || defined(_ARCH_PPC64) || \
+    defined(__POWERPC__) || defined(__PPC) || defined(__PPC__) || defined(__POWERPC) || \
+    defined(__PPC64) || defined(__PPC64__) || defined(__POWERPC64__)
+
+    #define CPU_PPC 1
+
+    #if defined(__ppc64) || defined(__ppc64__) || defined(__powerpc64) || defined(__powerpc64__) || \
+        defined(_ARCH_PPC64) || defined(__PPC64) || defined(__PPC64__) || defined(__POWERPC64__)
+        #define CPU_PPC64 1
+    #else
+        #define CPU_PPC32 1
+    #endif
+#endif
+
+#if defined(__mips) || defined(__mips__) || defined(__mips64) || defined(__mips64__)
+    #define CPU_MIPS 1
+
+    #if defined(__mips64) || defined(__mips64__)
+        #define CPU_MIPS64 1
+    #else
+        #define CPU_MIPS32 1
+    #endif
+#endif
+
+#if defined(__riscv) || defined(__riscv__)
+    #define CPU_RISCV 1
+
+    #if defined(__riscv_xlen)
+        #if __riscv_xlen == 64
+            #define CPU_RISCV64 1
+        #elif __riscv_xlen == 32
+            #define CPU_RISCV32 1
+        #endif
+    #endif
+#endif
 
 //
 // Determine the platform.
