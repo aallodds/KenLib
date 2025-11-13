d135d848cae93b828b5f66191705835ed03a2e66
grant <abernathy.grant@icloud.com>
2025-11-12 18:32:33 -0600

    KenLib header

---
+++ KenLib.h
@@ -0,0 +1,9 @@
+#ifndef KENLIB_H
+#define KENLIB_H
+
+#include <KenLib/Rules.h>
+#include <KenLib/BuildConfiguration.h>
+#include <KenLib/TargetConfiguration.h>
+#include <KenLib/Types.h>
+
+#endif // KENLIB_H
